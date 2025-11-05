74a996c0f6bf2237c8681d2e6c5078c995582a63
Shkhadija <xediceserif@gmail.com>
2025-11-05 11:23:30 +0400

    panel_yaratma

---
+++ panel_yaratma/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace panel_yaratma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            picBomba = new PictureBox();
+            panel = new Label();
+            btnYeniOyun = new Button();
+            btnPanelYarat = new Button();
+            lblPanelSay = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)picBomba).BeginInit();
+            SuspendLayout();
+            // 
+            // picBomba
+            // 
+            picBomba.Image = (Image)resources.GetObject("picBomba.Image");
+            picBomba.Location = new Point(91, 133);
+            picBomba.Name = "picBomba";
+            picBomba.Size = new Size(93, 62);
+            picBomba.SizeMode = PictureBoxSizeMode.StretchImage;
+            picBomba.TabIndex = 0;
+            picBomba.TabStop = false;
+            // 
+            // panel
+            // 
+            panel.AutoSize = true;
+            panel.BackColor = SystemColors.Control;
+            panel.Location = new Point(503, 361);
+            panel.Name = "panel";
+            panel.Size = new Size(73, 20);
+            panel.TabIndex = 1;
+            panel.Text = "Panel sayi";
+            // 
+            // btnYeniOyun
+            // 
+            btnYeniOyun.BackColor = Color.FromArgb(0, 64, 64);
+            btnYeniOyun.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnYeniOyun.Location = new Point(235, 350);
+            btnYeniOyun.Name = "btnYeniOyun";
+            btnYeniOyun.Size = new Size(107, 42);
+            btnYeniOyun.TabIndex = 2;
+            btnYeniOyun.Text = "Yeni oyun";
+            btnYeniOyun.UseVisualStyleBackColor = false;
+            btnYeniOyun.Click += btnYeniOyun_Click;
+            // 
+            // btnPanelYarat
+            // 
+            btnPanelYarat.BackColor = Color.FromArgb(0, 192, 192);
+            btnPanelYarat.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnPanelYarat.Location = new Point(27, 350);
+            btnPanelYarat.Name = "btnPanelYarat";
+            btnPanelYarat.Size = new Size(104, 42);
+            btnPanelYarat.TabIndex = 3;
+            btnPanelYarat.Text = "Panel yarat";
+            btnPanelYarat.UseVisualStyleBackColor = false;
+            btnPanelYarat.Click += btnPanelYarat_Click;
+            // 
+            // lblPanelSay
+            // 
+            lblPanelSay.Location = new Point(582, 358);
+            lblPanelSay.Name = "lblPanelSay";
+            lblPanelSay.Size = new Size(125, 27);
+            lblPanelSay.TabIndex = 4;
+            lblPanelSay.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.AppWorkspace;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblPanelSay);
+            Controls.Add(btnPanelYarat);
+            Controls.Add(btnYeniOyun);
+            Controls.Add(panel);
+            Controls.Add(picBomba);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)picBomba).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox picBomba;
+        private Label panel;
+        private Button btnYeniOyun;
+        private Button btnPanelYarat;
+        private TextBox lblPanelSay;
+    }
+}
---
+++ panel_yaratma/Form1.cs
@@ -0,0 +1,60 @@
+﻿namespace panel_yaratma
+{
+    public partial class Form1 : Form
+    {
+        int panelSayi = 0;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPanelYarat_Click(object sender, EventArgs e)
+        {
+            Panel p = new Panel();
+            p.Size = new Size(50, 50);
+            p.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            p.Left = rnd.Next(0, this.ClientSize.Width - p.Width);
+            p.Top = rnd.Next(0, this.ClientSize.Height - p.Height - 100);
+
+            this.Controls.Add(p);
+            panelSayi++;
+            panel.Text = panelSayi.ToString();
+
+
+            if (p.Bounds.IntersectsWith(picBomba.Bounds))
+            {
+                MessageBox.Show("💥 Oyun bitdi!");
+                OyunuDayandir();
+            }
+        }
+
+        private void btnYeniOyun_Click(object sender, EventArgs e)
+        {
+            for (int i = this.Controls.Count - 1; i >= 0; i--)
+            {
+                if (this.Controls[i] is Panel && this.Controls[i].Name != "picBomba")
+                {
+                    this.Controls.RemoveAt(i);
+                }
+            }
+
+            panelSayi = 0;
+            panel.Text = "0";
+
+            picBomba.Left = rnd.Next(0, this.ClientSize.Width - picBomba.Width);
+            picBomba.Top = rnd.Next(0, this.ClientSize.Height - picBomba.Height - 100);
+        }
+
+        private void OyunuDayandir()
+        {
+            btnPanelYarat.Enabled = false;
+        }
+    }
+
+}
